fcf44dd015a370ed5774dfabea3850362b8a64e4
jairovera17 <jairov1995@yahoo.com>
2017-06-22 04:15:23 -0500

    hola

--- frontEnd/frontLabCalendar/src/app/app.module.ts
+++ frontEnd/frontLabCalendar/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
