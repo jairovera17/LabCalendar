eb567f8db172cc7e95137a4643c9570442ba3a14
jairovera17 <jairov1995@yahoo.com>
2017-06-24 13:58:05 -0500

    jairo commit

---
+++ frontEnd/frontLabCalendar/src/app/week-view-calendar/week-view-calendar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-week-view-calendar',
+  templateUrl: './week-view-calendar.component.html',
+  styleUrls: ['./week-view-calendar.component.css']
+})
+export class WeekViewCalendarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
