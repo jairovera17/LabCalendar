3352d75de18300e0552b513bbb41e1c22e9dd339
jairovera17 <jairov1995@yahoo.com>
2017-06-27 22:39:32 -0500

    Inclusion de Router y mejoras visuales

---
+++ frontEnd/frontLabCalendar/src/app/lab-view/lab-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lab-view',
+  templateUrl: './lab-view.component.html',
+  styleUrls: ['./lab-view.component.css']
+})
+export class LabViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
